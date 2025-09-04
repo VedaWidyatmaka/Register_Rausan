32563f6a09ebd1726df9b184b8411858e8d44e28
VedaWidyatmaka <rausanmawlasa@gmail.com>
2025-09-04 10:45:48 +0700

    Create login.js

---
+++ Register_Compiled/Register_Login/login.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    const loginEmail = document.getElementById("loginEmail").value;
+    const loginPassword = document.getElementById("loginPassword").value;
+    
+    const storedUser = JSON.parse(localStorage.getItem(loginEmail));
+    
+    if (storedUser && storedUser.password == loginPassword) {
+        alert("Login Berhasil" + storedUser.nama + "!");
+    }else {
+        alert("Email atau password salah")
+    }
+});
